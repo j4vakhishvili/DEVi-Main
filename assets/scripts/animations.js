b2a16409ccef52d956e129194cd001052b63d0e6
J4vakhishvili <n1kc0nnyt@gmail.com>
2021-10-10 13:38:12 +0400

    Update animations.js

--- assets/scripts/animations.js
+++ assets/scripts/animations.js
@@ -7,6 +7,13 @@ const b2 = document.querySelector(".b2");
 const b3 = document.querySelector(".b3");
 const b4 = document.querySelector(".b4");
 const b5 = document.querySelector(".b5");
+const b6 = document.querySelector(".b6");
+const b7 = document.querySelector(".b7");
+const b8 = document.querySelector(".b8");
+const b9 = document.querySelector(".b9");
+const b10 = document.querySelector(".b10");
+const b11 = document.querySelector(".b11");
+const b12 = document.querySelector(".b12");
 
 window.addEventListener("load", () => {
     header.style.transform = "translateX(0)";
@@ -20,4 +27,11 @@ window.addEventListener("load", () => {
     b3.style.transform = "scale(1)";
     b4.style.transform = "scale(1)";
     b5.style.transform = "scale(1)";
+    b6.style.transform = "scale(1)";
+    b7.style.transform = "scale(1)";
+    b8.style.transform = "scale(1)";
+    b9.style.transform = "scale(1)";
+    b10.style.transform = "scale(1)";
+    b11.style.transform = "scale(1)";
+    b12.style.transform = "scale(1)";
 });
